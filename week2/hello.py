544ba0cc5944d36526b7f86516f0f59b4a98f96a
CyOps <stuartcjoseph0@gmail.com>
2026-07-18 17:20:48 +0200

    chore: started Week 2 - basic Python variables and scripts

---
+++ week2/hello.py
@@ -0,0 +1,2 @@
+print("Hello from a Python file!")
+print("This is Week 2 of my cloud journey.")
---
+++ week2/variables.py
@@ -0,0 +1,9 @@
+name = "Stuart"
+age = 30
+price = 2500.50
+is_active = True
+
+print("Name:", name)
+print("Age:", age)
+print("Price:", price)
+print("Active:", is_active)
